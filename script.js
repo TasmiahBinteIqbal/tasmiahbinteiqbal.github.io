b41b459890d0b202c7a14bd26e61ec15c28ab9b8
TasmiahBinteIqbal <tasmiah00021@gmail.com>
2026-05-25 04:51:29 +0600

    script

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('Hello!');
+// Smooth Scroll
+const links = document.querySelectorAll('a[href^="#"]');
+
+links.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
